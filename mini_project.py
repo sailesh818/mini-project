0bc025aa339b5b887c8c6aaa7dd5d2e77bef3cdc
sailesh818 <sailesh7019@gmail.com>
2025-07-11 18:58:44 +0545

    bug fixes

--- mini_project.py
+++ mini_project.py
@@ -1,45 +1,28 @@
 import pygame
 import sys
-
 pygame.init()
-
 WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Mini Paint App")
-
-
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED   = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE  = (0, 0, 255)
 PURPLE = (128, 0, 128)
-
-
 screen.fill(WHITE)
-
-
 brush_color = BLACK
 brush_size = 5
 drawing = False
-
-
 running = True
 while running:
     for event in pygame.event.get():
-        
         if event.type == pygame.QUIT:
             running = False
-
-        
         elif event.type == pygame.MOUSEBUTTONDOWN:
             drawing = True
-
-        
         elif event.type == pygame.MOUSEBUTTONUP:
             drawing = False
-
-        
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_r:
                 brush_color = RED
@@ -55,15 +38,9 @@ while running:
                 screen.fill(WHITE)  
             elif event.key == pygame.K_ESCAPE:
                 running = False
-
-    
     if drawing:
         mouse_pos = pygame.mouse.get_pos()
         pygame.draw.circle(screen, brush_color, mouse_pos, brush_size)
-
-    
     pygame.display.update()
-
-
 pygame.quit()
 sys.exit()
--- text.py
+++ text.py
@@ -1,35 +1,23 @@
 import pygame
 import sys
-
 pygame.init()
-
 width, height = 800, 400
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Display Text on screen")
-
-
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
-
 font = pygame.font.SysFont(None, 36)
-
 text_surface = font.render("Hello Pygame!", True, BLUE)
-
 text_rect = text_surface.get_rect(center =(width//2, height//2))
-
 running = True
 while running:
     screen.fill(WHITE)
-    
     screen.blit(text_surface, text_rect)
-    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-            
+            running = False    
     pygame.display.update()
-    
 pygame.quit()
 sys.exit()
     
